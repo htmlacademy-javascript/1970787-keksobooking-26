92e261ac07d95a260083fe30785161b37779c650
LorDoCH <mr.khibenkov@gmail.com>
2022-06-02 17:16:37 +0300

    Добавляет функцию приведения отрицательного к нулю

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-function getRandomInt(firstNumber, secondNumber) {  //Максимум не включается, минимум включается
+function getNegativeToZero(number) {
+  if (number < 0) {
+    number = 0;
+  }
+  return number;
+}
+
+function getRandomInteger(firstNumber, secondNumber) {  //Максимум не включается, минимум включается
+  firstNumber = getNegativeToZero(firstNumber);
+  secondNumber = getNegativeToZero(secondNumber);
+
   if (firstNumber === secondNumber) {
     return firstNumber;
   }
@@ -16,4 +26,27 @@ function getRandomInt(firstNumber, secondNumber) {  //Максимум не вк
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-getRandomInt(15.258,10.147);
+getRandomInteger(15.258, 10.147); //временное использование - потом удалить
+
+function getRandomFloating(firstNumber, secondNumber, symbolsAfterDot) {  //Максимум не включается, минимум включается
+  firstNumber = getNegativeToZero(firstNumber);
+  secondNumber = getNegativeToZero(secondNumber);
+  symbolsAfterDot = getNegativeToZero(symbolsAfterDot);
+
+  if (firstNumber === secondNumber) {
+    return firstNumber;
+  }
+
+  let min = firstNumber;
+  let max = secondNumber;
+
+  if (firstNumber > secondNumber) {
+    min = secondNumber;
+    max = firstNumber;
+  }
+
+  const calcRandomNumber = Math.random() * (max - min) + min;
+  return calcRandomNumber.toFixed(symbolsAfterDot);
+}
+
+getRandomFloating(-15.258,10.147, 5); //временное использование - потом удалить

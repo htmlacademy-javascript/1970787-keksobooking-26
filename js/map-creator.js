e3c5173ca21dc1d4c3553ab83651313eba6243c7
LorDoCH <mr.khibenkov@gmail.com>
2022-07-05 19:10:14 +0300

    Добавляет ввод координат в поле адреса

--- js/map-creator.js
+++ js/map-creator.js
@@ -1,5 +1,7 @@
 import {getPageEnabled} from './page-status-toggler.js';
 
+const addressField = document.querySelector('#address');
+
 const map = L.map('map-canvas')
   .on('load', () => {
     getPageEnabled();
@@ -15,3 +17,28 @@ L.tileLayer(
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
 ).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const marker = L.marker(
+  {
+    lat: 35.6895000,
+    lng: 139.6917100,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  }
+);
+
+marker.addTo(map);
+
+marker.on('moveend', (evt) => {
+  addressField.value = `${evt.target.getLatLng().lat.toFixed(5)}, ${evt.target.getLatLng().lng.toFixed(5)}`;
+});
+
+

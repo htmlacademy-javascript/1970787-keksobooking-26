99e3d13697ab20ce494ac458b65f9ddce8e7020a
LorDoCH <mr.khibenkov@gmail.com>
2022-07-19 15:35:06 +0300

    Добавляет фильтр по цене

--- js/ads-filter.js
+++ js/ads-filter.js
@@ -3,30 +3,39 @@ import {showAlert} from './util.js';
 import {getAdsPoints} from './map-creator.js';
 
 const CARDS_LIMIT = 10;
+const LOW_PRICE = 10000;
+const HIGH_PRICE = 50000;
 
 const mapFiltersForm = document.querySelector('.map__filters');
 const housingTypeField = mapFiltersForm.querySelector('#housing-type');
 const housingRoomsField = mapFiltersForm.querySelector('#housing-rooms');
+const housingGuestsField = mapFiltersForm.querySelector('#housing-guests');
+const housingPriceField = mapFiltersForm.querySelector('#housing-price');
 
-const filterAdsType = (ads) =>{
-  if (housingTypeField.value !== 'any') {
-    return ads.offer.type === housingTypeField.value;
-  } else {
-    return true;
-  }
-};
+const filterAdsType = (ads) => housingTypeField.value !== 'any' ? ads.offer.type === housingTypeField.value : true;
 
-const filterAdsRooms = (ads) =>{
-  if (housingRoomsField.value !== 'any') {
-    return ads.offer.rooms === +housingRoomsField.value;
-  } else {
-    return true;
-  }
+const filterAdsRooms = (ads) => housingRoomsField.value !== 'any' ? ads.offer.rooms === +housingRoomsField.value : true;
+
+const filterAdsGuests = (ads) => housingGuestsField.value !== 'any' ? ads.offer.guests === +housingGuestsField.value : true;
+
+const filterAdsPrice = (ads) => {
+  const adsPrice = () => {
+    if (ads.offer.price < LOW_PRICE) {
+      return 'low';
+    } else {
+      if (ads.offer.price > HIGH_PRICE) {
+        return 'high';
+      } else {
+        return 'middle';
+      }
+    }
+  };
+  return housingPriceField.value !== 'any' ? adsPrice() === housingPriceField.value : true;
 };
 
 mapFiltersForm.addEventListener('change', () => {
   getAdsData((ads) => {
-    const adsList = ads.filter(filterAdsType).filter(filterAdsRooms).slice(0, CARDS_LIMIT);
+    const adsList = ads.filter(filterAdsType).filter(filterAdsRooms).filter(filterAdsGuests).filter(filterAdsPrice).slice(0, CARDS_LIMIT);
     getAdsPoints(adsList);
   }, showAlert);
 });
